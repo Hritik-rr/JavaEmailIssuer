0cbba9326e8ce0d537705bc13ec19105627ab324
HRX_ <41600508+Hritik-rr@users.noreply.github.com>
2021-08-29 00:36:04 +0530

    Update JavaMain.java

--- src/javaemailapplication/JavaMain.java
+++ src/javaemailapplication/JavaMain.java
@@ -2,6 +2,6 @@ package javaemailapplication;
 
 public class JavaMain {
     public static void main(String[] args)throws Exception{
-        JavaMailUtil.sendMail("yagya.madrit@gmail.com");
+        JavaMailUtil.sendMail("xyz@gmail.com");
     }
 }
